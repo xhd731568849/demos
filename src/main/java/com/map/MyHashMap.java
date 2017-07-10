52e13d6868ef385419b6cc3d092dae448d725c5d
xhd731568849 <xhd731568849@163.com>
2017-07-11 00:12:08 +0800

    编写部分代码

--- src/main/java/com/map/MyHashMap.java
+++ src/main/java/com/map/MyHashMap.java
@@ -1,13 +1,69 @@
 package com.map;
 
+import java.util.Map;
+
 /**
  * Created by Administrator on 2017/7/10/010.
  */
-public class MyHashMap implements MyMap {
+public class MyHashMap<K,V> implements MyMap<K,V> {
 
-    private int size = 1;
-    //sdfasdsfd
+    private static int defaultLength = 1<<4;
 
-    //asds
-    public MyHashMap(){}
+    private static double defaultAddSizeFactor = 0.75;
+
+    private int useSize;
+
+    private Entry<K,V>[] table = null;
+
+    public MyHashMap(){
+        this(defaultLength,defaultAddSizeFactor);
+    }
+
+    public MyHashMap(int length , double factor){
+        if(length < 0){
+            throw new IllegalArgumentException("参数不能为负数："+length);
+        }
+        if(factor < 0 || Double.isNaN(factor)){
+            throw new IllegalArgumentException("扩容必须是大于零的数字："+factor);
+        }
+        this.defaultLength = length;
+        this.defaultAddSizeFactor = factor;
+        table = new Entry[defaultLength];
+    }
+
+    class Entry<K,V> implements Map.Entry<K,V>{
+        K k;
+        V v;
+        Entry<K,V> next;
+
+        public K getKey() {
+            return k;
+        }
+
+        public V getValue() {
+            return v;
+        }
+
+        public V setValue(V value) {
+            return null;
+        }
+    }
+
+    public V put(K k, V v) {
+        //存之前判断是否要扩容
+        if(useSize > defaultLength*defaultAddSizeFactor){
+            //扩容2倍
+            up2Size();
+        }
+        return null;
+    }
+
+    private void up2Size() {
+        
+    }
+
+    public V get(K k) {
+        return null;
+    }
+
 }
--- src/main/java/com/map/MyMap.java
+++ src/main/java/com/map/MyMap.java
@@ -3,7 +3,15 @@ package com.map;
 /**
  * Created by Administrator on 2017/7/10/010.
  */
-public interface MyMap {
-    //sasdfadf
+public interface MyMap<K,V> {
+
+    public V put(K k , V v);
+
+    public V get(K k);
+
+    public interface Entry<K,V>{
+        public K getKey();
+        public V getValue();
+    }
 
 }
