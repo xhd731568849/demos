0badee2e74ed8948d8a7689d68d4bb4da4ca4918
xhd731568849 <xhd731568849@163.com>
2017-07-10 23:54:54 +0800

    测一下

---
+++ src/main/java/com/map/MyHashMap.java
@@ -0,0 +1,13 @@
+package com.map;
+
+/**
+ * Created by Administrator on 2017/7/10/010.
+ */
+public class MyHashMap implements MyMap {
+
+    private int size = 1;
+    //sdfasd
+
+    //asds
+    public MyHashMap(){}
+}
---
+++ src/main/java/com/map/MyMap.java
@@ -0,0 +1,9 @@
+package com.map;
+
+/**
+ * Created by Administrator on 2017/7/10/010.
+ */
+public interface MyMap {
+    //sasdfadf
+
+}
