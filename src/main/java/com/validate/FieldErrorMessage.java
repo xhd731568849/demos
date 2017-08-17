5c00262cef1b58e9fa285582766f935567f912ce
xhd731568849 <xhd731568849@163.com>
2017-08-18 00:44:41 +0800

    加入FieldErrorMessage

---
+++ src/main/java/com/validate/FieldErrorMessage.java
@@ -0,0 +1,22 @@
+package com.validate;
+
+/**
+ * Created by xuhandong on 2017/8/17/017.
+ */
+public class FieldErrorMessage {
+    private String msg;
+
+    public FieldErrorMessage(){}
+
+    public FieldErrorMessage(String msg){
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
